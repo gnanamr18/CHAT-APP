4b18afb57f12c1eb62e9df9c2f48c13936afd36d
gnanamr18 <gnanam.r18@gmail.com>
2024-01-17 09:39:50 +0530

    doing front ui

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,32 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const path = require("path");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 //Load env var
-dotenv.config({ path: "./config/config.env" });
+dotenv.config({ path: "./config/.env" });
+console.log(process.env);
 
 const app = express();
+connectDB();
 
-const PORT = process.env.PORT;
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, "../public")));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var messages = mongoose.model("messages", { name: String, message: String });
+
+app.get("/messages", (req, res) => {
+  res.send("api running");
+  res.status(200).json({ message: success });
+});
 
 app.listen(PORT, () => {
   console.log(`server running in ${process.env.NODE_ENV} on the PORT ${PORT}`);
